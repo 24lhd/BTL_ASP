a58c6df799cf7d3621dfa319886df4f29438bd14
Dương Lê <24duong@gmail.com>
2017-08-16 03:39:18 +0700

    list bao theo the loai

--- bao/theloai.aspx.cs
+++ bao/theloai.aspx.cs
@@ -6,35 +6,60 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
-public partial class bao_xembao : System.Web.UI.Page
+public partial class bao_theloai : System.Web.UI.Page
 {
     string connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\DocBaoV01.mdf;Integrated Security=True;Connect Timeout=30";
     protected void Page_Load(object sender, EventArgs e)
     {
+
         string v = Request.QueryString["id"];
         if (v != null)
         {
             if (!IsPostBack)
             {
-              
-                DataView testView = (DataView)dsXemBao.Select(DataSourceSelectArguments.Empty);
-                LoadData(v);
+
        
+                LoadData(v);
+
             }
         }
-       
     }
     private void LoadData(string idBao)
     {
-        string text = "SELECT * FROM BaiBao WHERE MaBaiBao = '" + idBao+"'";
-        try {
+        string text = "SELECT * FROM BaiBao WHERE MaTheLoai = '" + idBao + "'";
+        try
+        {
             SqlConnection conn = new SqlConnection(this.connectionString);
             SqlCommand command = new SqlCommand(text, conn);
             conn.Open();
             DataTable dataTable = new DataTable();
             SqlDataAdapter da = new SqlDataAdapter(command);
             da.Fill(dataTable);
-            string Title = dataTable.Rows[0]["Title"].ToString();
+            int flag = 1;
+         
+            string rowItem="";
+            string OneRow = "";
+          
+            txtTenTheLoai.Text = "<i>" + idBao + "</i>";
+            foreach (DataRow row in dataTable.Rows)
+            {
+           
+                string Image = row["Image"].ToString();
+                string Title = row["Title"].ToString();
+                string MoTaNgan = row["MoTaNgan"].ToString();
+                string NgayDang = row["NgayDang"].ToString();
+                string item = "<div class=\"w3-row w3-margin\"><div class=\"w3-third\"><img src=\"/public/image/"+
+                       Image
+                       +"\" style=\"width: 100%;\"> </div><div class=\"w3-twothird w3-container\"><h2>"+Title
+                           +"</h2><p>" +
+              MoTaNgan  +"</p> </div></div>";
+              
+                rowItem = rowItem + item;
+             
+                
+            }
+            contentList.Text = rowItem;
+           /* string Title = dataTable.Rows[0]["Title"].ToString();
             txth2.Text = Title;
             string Image = dataTable.Rows[0]["Image"].ToString();
             imBao.ImageUrl = "~/public/image/" + Image;
@@ -43,14 +68,14 @@ public partial class bao_xembao : System.Web.UI.Page
             string Username = dataTable.Rows[0]["Username"].ToString();
             txtTacGia.Text = Username;
             string NgayDang = dataTable.Rows[0]["NgayDang"].ToString();
-            txtTime.Text = NgayDang;
+            txtTime.Text = NgayDang;*/
             conn.Close();
         }
-        catch (Exception exe)
+        catch (Exception e)
         {
-            txtcontent.Text = "Not found 404";
+         //   txtcontent.Text = "Not found 404";
         }
-      
-    
-        }
+
+
+    }
 }
